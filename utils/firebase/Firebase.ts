f7c138204f3f35829cca8d712b010bf7a741b9cf
NiclasDev63 <niclas,gregor20@gmail.com>
2022-10-02 23:44:20 +0200

    working local now test in production

--- utils/firebase/Firebase.ts
+++ utils/firebase/Firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "process.env.APIKEY",
-  authDomain: "process.env.AUTHDOMAIN",
-  databaseURL:
-    "process.env.DATABASEURL",
-  projectId: "process.env.PROJECTID",
-  storageBucket: "process.env.STORAGEBUCKET",
-  messagingSenderId: "process.env.MESSAGINGSENDERID",
-  appId: "process.env.APPID",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
